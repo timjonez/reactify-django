e60b45d86b34763c84aa11a701a55bc823f74980
Tim Jones <timjonez@protonmail.com>
2020-05-19 18:19:37 -0400

    init

---
+++ src/reactify-ui/src/posts/PostInline.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class PostInline extends Component {
+  render() {
+
+    const {post} = this.props
+    const {elClass} = this.props
+    const showContent = elClass === 'card' ? 'd-block': 'd-none'
+    return (
+      <div className={elClass}>
+        <h1>{post.title}</h1>
+        <p className={showContent}>{post.content}</p>
+      </div>
+    );
+  }
+}
+
+export default PostInline;
